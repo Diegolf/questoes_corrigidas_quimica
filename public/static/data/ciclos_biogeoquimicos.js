539055c6df9643551fa8ffeb893546d951bbbcb0
Diego Leonardo <diegolf125@gmail.com>
2018-09-09 17:16:37 -0300

    adicionando novas questões

---
+++ public/static/data/ciclos_biogeoquimicos.js
@@ -0,0 +1,15 @@
+{
+  "tema":"Ciclos Biogeoquímicos",
+  "questoes":[
+    {
+      "enunciado":"<strong>Questão 01 (Questão 6 – 2009)</strong><br/>&emsp; O ciclo biogeoquímico do carbono compreende diversos compartimentos, entre os quais a Terra, a atmosfera e os oceanos, e diversos processos que permitem a transferência de compostos entre esses reservatórios. Os estoques de carbono armazenados na forma de recursos não renováveis, por exemplo, o petróleo, são limitados, sendo de grande relevância que se perceba a importância da substituição de combustíveis fósseis por combustíveis de fontes renováveis.<br/>A utilização de combustíveis fósseis interfere no ciclo do carbono, pois provoca ",
+      "a":"Aumento da porcentagem de carbono contido na Terra.",
+      "b":"Redução na taxa de fotossíntese dos vegetais superiores.",
+      "c":"Aumento da produção de carboidratos de origem vegetal.",
+      "d":"Aumento na quantidade de carbono presente na atmosfera.",
+      "e":"Redução da quantidade global de carbono armazenado nos oceanos.",
+      "letra_resposta":"A",
+      "explicacao_resposta":""
+    }
+  ]
+}
---
+++ public/static/data/eletroquimica.js
@@ -0,0 +1,15 @@
+{
+  "tema":"Eletroquímica",
+  "questoes":[
+    {
+      "enunciado":"<strong>Questão 01 (Questão 15 – 2009)</strong><br/>&emsp; Para que apresente condutividade elétrica adequada a muitas aplicações, o cobre bruto obtido por métodos térmicos é purificado eletroliticamente. Nesse processo, o cobre bruto impuro constitui o ânodo da célula, que está imerso em uma solução de CuSO<sub>4</sub>. À medida que o cobre impuro é oxidado no ânodo, íons Cu<sup>2+</sup> da solução são depositados na forma pura no cátodo. Quanto às impurezas metálicas, algumas são oxidadas, passando à solução, enquanto outras simplesmente se desprendem do ânodo e se sedimentam abaixo dele. As impurezas sedimentadas são posteriormente processadas, e sua comercialização gera receita que ajuda a cobrir os custos do processo. A série eletroquímica a seguir lista o cobre e alguns metais presentes como impurezas no cobre bruto de acordo com suas forças redutoras relativas. <img src=\"static/imgs/eletroquimica1.jpeg\" class=\"w3-center\"> <br/> Entre as impurezas metálicas que constam na série apresentada, as que se sedimentam abaixo do ânodo de cobre são",
+      "a":"Au, Pt, Ag, Zn, Ni e Pb.",
+      "b":"Au, Pt e Ag.",
+      "c":"Zn, Ni e Pb.",
+      "d":"Au e Zn.",
+      "e":"Ag e Pb.",
+      "letra_resposta":"A",
+      "explicacao_resposta":"Cátodo é o eletrodo no qual ocorre o processo de redução, então há o ganho de elétrons. Ânodo é o eletrodo no qual ocorre o processo de oxidação, ou seja, há perda de elétrons (PERUZZO; CANTO, 2006). Como apresentado na figura acima, os elementos estão em ordem crescente de força redutora. A força redutora é a capacidade de uma espécie química provocar a redução em outra espécie, e consequentemente sofrer oxidação. Portanto, fazendo-se uma comparação, podemos perceber que os elementos que se sedimentam abaixo do ânodo cobre são, o ouro a platina e a prata, pois, esses elementos possuem força redutora menor, sofrendo redução."
+    }
+  ]
+}
