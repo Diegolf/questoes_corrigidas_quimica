6387c3854203583a4dd93e9b9a5c0540003263d3
Diego Leonardo <diegolf125@gmail.com>
2018-09-09 17:23:40 -0300

    adicionando novas questões

--- public/static/data/ciclos_biogeoquimicos.js
+++ public/static/data/ciclos_biogeoquimicos.js
@@ -2,7 +2,7 @@
   "tema":"Ciclos Biogeoquímicos",
   "questoes":[
     {
-      "enunciado":"<strong>Questão 01 (Questão 6 – 2009)</strong><br/>&emsp; O ciclo biogeoquímico do carbono compreende diversos compartimentos, entre os quais a Terra, a atmosfera e os oceanos, e diversos processos que permitem a transferência de compostos entre esses reservatórios. Os estoques de carbono armazenados na forma de recursos não renováveis, por exemplo, o petróleo, são limitados, sendo de grande relevância que se perceba a importância da substituição de combustíveis fósseis por combustíveis de fontes renováveis.<br/>A utilização de combustíveis fósseis interfere no ciclo do carbono, pois provoca ",
+      "enunciado":"<strong>Questão 01 (Questão 6 – 2009)</strong><br/>&emsp; O ciclo biogeoquímico do carbono compreende diversos compartimentos, entre os quais a Terra, a atmosfera e os oceanos, e diversos processos que permitem a transferência de compostos entre esses reservatórios. Os estoques de carbono armazenados na forma de recursos não renováveis, por exemplo, o petróleo, são limitados, sendo de grande relevância que se perceba a importância da substituição de combustíveis fósseis por combustíveis de fontes renováveis.<br/>A utilização de combustíveis fósseis interfere no ciclo do carbono, pois provoca: ",
       "a":"Aumento da porcentagem de carbono contido na Terra.",
       "b":"Redução na taxa de fotossíntese dos vegetais superiores.",
       "c":"Aumento da produção de carboidratos de origem vegetal.",
